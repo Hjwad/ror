364f26e66f9a046d1a6ca983341435a0c82aed2d
Hussien <158073869+Hjwad@users.noreply.github.com>
2024-06-21 23:16:51 +0300

    Update zoz.py

--- zoz.py
+++ zoz.py
@@ -1,13 +1,10 @@
-import os
 from flask import Flask
-from flask_restful import Resource, Api
 
 app = Flask(__name__)
-api = Api(app)
 
-class Greeting (Resource):
-    def get(self):
-        return "welcome"
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
-api.add_resource(Greeting, '/')
-app.run(host="0.0.0.0", port=os.environ.get("PORT", 5000))
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
